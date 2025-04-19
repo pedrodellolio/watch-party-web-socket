7275615f62362ad16825542bce2eef0246041266
Pedro Dell'Olio <pedrohdellolio@gmail.com>
2025-04-19 03:09:18 -0300

    Add supabase auth

--- Program.cs
+++ Program.cs
@@ -1,17 +1,54 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Npgsql;
 using System.Data;
 using System.Net.WebSockets;
+using System.Text;
 using WatchParty.WS.Managers;
 using WatchParty.WS.Repositories.UserRepository;
 using WatchParty.WS.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddScoped<IDbConnection>(sp => new NpgsqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var bytes = Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("JwtKey")!);
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration.GetValue<string>("Issuer"),
+        ValidAudience = builder.Configuration.GetValue<string>("Audience"),
+        IssuerSigningKey = new SymmetricSecurityKey(bytes),
+    };
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            var path = context.HttpContext.Request.Path;
+            if (path.StartsWithSegments("/ws") &&
+                context.Request.Query.TryGetValue("token", out var token))
+            {
+                context.Token = token;
+            }
+            return Task.CompletedTask;
+        }
+    };
+});
+
+builder.Services.AddScoped<IDbConnection>(sp => new NpgsqlConnection(builder.Configuration.GetValue<string>("ConnectionStrings:DefaultConnection")));
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddTransient<IYoutubeService, YoutubeService>();
 builder.Services.AddSingleton<RoomManager>();
 
 var app = builder.Build();
+app.UseAuthentication();
 
 app.UseWebSockets();
 
@@ -19,6 +56,12 @@ app.Map("/ws", async context =>
 {
     if (context.WebSockets.IsWebSocketRequest)
     {
+        if (!context.User.Identity?.IsAuthenticated ?? true)
+        {
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            return;
+        }
+
         WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
         string? roomId = context.Request.Query["room"];
         string? userId = context.Request.Query["userId"];
